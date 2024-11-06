4a4a4364aa6c62edb4bdc2414a60c24dda97f611
vaishali023 <vaishalichaudhary011@gmail.com>
2024-11-06 05:41:13 -0500

    Refractor: Update vite.config file

--- qbit-explorer/vite.config.ts
+++ qbit-explorer/vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: './', 
+  build: {
+    target: 'esnext',  // Ensure compatibility with modern ES modules
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
 })
